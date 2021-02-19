474b06e51bec7c5a07e27a908b1983ff6b1d303f
Aziz Sudrajat <azizsudrajat@MACs-MacBook-Pro.local>
2021-02-19 21:45:15 +0700

    add model table flip

---
+++ application/models/BankFlipModel.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class BankFlipModel extends MY_Model {
+	protected $table = 'bank_flip';
+}
+?>
---
+++ application/models/BankUsersModel.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class BankUsersModel extends MY_Model {
+	protected $table = 'bank_users';
+}
+?>
